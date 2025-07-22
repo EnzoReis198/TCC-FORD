0fa001b4cd40c99ac0ad7ebc0d8756ed80adfe7a
EnzoReis198 <enzoreis198@gmail.com>
2025-07-22 02:28:05 -0300

    Corrigindo rotas e configurando serverless

--- API/src/rotas.js
+++ API/src/rotas.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const rota = express()
-const bodyParser = require("body-parser")
+const express = require("express");
+const serverless = require("serverless-http"); // Adicione isso
+const rota = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
@@ -10,16 +10,13 @@ const login = require("./controllers/login");
 const validarLogin = require("./intermediarios/validarLogin");
 const { listarVeiculos, listarVeiculoID } = require("./controllers/veicles");
 
-
-
-rota.use(express.json()); 
-
-rota.use(cors())
-// rota.use(bodyParser.json());
+rota.use(express.json());
+rota.use(cors());
 
 rota.post('/login', login);
-rota.get('/veicles', listarVeiculos);
-rota.get('/veicles/:id', listarVeiculoID);
+rota.get('/vehicles', listarVeiculos);  // Corrigido para "vehicles"
+rota.get('/vehicles/:id', listarVeiculoID);  // Corrigido para "vehicles"
 
-
-module.exports = rota
+// Exporte a função serverless
+module.exports.handler = serverless(rota);  // Isso vai exportar a função Express para ser executada na Vercel
+module.exports.app = rota;  // Exporta o app para uso local ou testes
